a1d01b64ce2f3e9a7b9c1e28bbded60ab26af066
marouane <marohanter084@gmail.com>
2025-04-06 00:09:59 -0400

    home page

---
+++ frontend/src/components/Pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div className = "p-6">
+            <h1 className="text-3x1 font-bold">Welcome to Smart Ecocenter</h1>
+            <p className="mt-4 text-lg">We help find the right place to recycle based on your needs and location.</p>
+            <a href="/signup" className="text-blue-500 uderline">Get started</a>
+        </div>
+    );
+}
