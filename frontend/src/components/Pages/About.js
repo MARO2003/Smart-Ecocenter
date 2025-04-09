1207c7637f127b171b3945e0746b3b9e49035252
marouane <marohanter084@gmail.com>
2025-04-09 15:43:57 -0400

    les modifs

---
+++ frontend/src/components/Pages/About.js
@@ -0,0 +1,12 @@
+import Layout from "../Layout";
+export default function About(){
+    return(
+        <Layout>
+            <div className = "p-6">
+                <h1 className="text-3x1 font-bold">About</h1>
+                
+            </div>
+        </Layout>
+        
+    );
+}
---
+++ frontend/src/components/Pages/Locations.js
@@ -0,0 +1,12 @@
+import Layout from "../Layout";
+export default function Locations(){
+    return(
+        <Layout>
+            <div className = "p-6">
+                <h1 className="text-3x1 font-bold">Locations</h1>
+            </div>
+  
+        </Layout>
+        
+    );
+}
