3cc72ae95aa176436d6e0c0f9221766c78561759
marouane <marohanter084@gmail.com>
2025-04-11 14:35:37 -0400

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import React, {useEffect,useState} from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
@@ -6,16 +5,7 @@ import Contact from './components/Pages/Contact';
 import Locations from './components/Pages/Locations';
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
-  useEffect(()=>{
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data=>{
-        setBackendData(data)
-      }
-    )
-  },[])
+  
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
